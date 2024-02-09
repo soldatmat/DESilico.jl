2956eb1c6a463bfc8c4fadd8506a34bce19cac16
soldamatlab <matoussoldat@email.cz>
2024-02-09 15:18:00 +0100

    Add tests for DictScreeningWithDefault

--- test/unit/screening/dict_screening.jl
+++ test/unit/screening/dict_screening.jl
@@ -10,21 +10,71 @@
         ['H', 'D', 'G', 'V'] => 0.0264798489707,
         ['I', 'D', 'G', 'V'] => 1.4459050863,
     )
+    default = 0.0
+    missing_key = ['V', 'D', 'G', 'A']
 
-    @testset "from Dict" begin
-        sd_from_dict = DESilico.DictScreening(fitness_dict)
+    @testset "DictScreening" begin
+        @testset "from Dict" begin
+            ds = DESilico.DictScreening(fitness_dict)
 
-        for key in keys(fitness_dict)
-            @test sd_from_dict(key) == fitness_dict[key]
+            @test typeof(ds) == DESilico.DictScreening
+            for key in keys(fitness_dict)
+                @test ds(key) == fitness_dict[key]
+            end
+            try
+                ds(missing_key)
+            catch e
+                @test e == KeyError(missing_key) 
+            end
+        end
+
+        @testset "from file" begin
+            file_path = "unit/screening/data/test_data.xlsx"
+            ds = DESilico.DictScreening(file_path)
+
+            @test typeof(ds) == DESilico.DictScreening
+            for key in keys(fitness_dict)
+                @test ds(key) == fitness_dict[key]
+            end
+            try
+                ds(missing_key)
+            catch e
+                @test e == KeyError(missing_key)
+            end
         end
     end
 
-    @testset "from file" begin
-        file_path = "unit/screening/data/test_data.xlsx"
-        sd_from_file = DESilico.DictScreening(file_path)
-                
-        for key in keys(fitness_dict)
-            @test sd_from_file(key) == fitness_dict[key]
+    @testset "DictScreeningWithDefault" begin
+        @testset "direct constructor" begin
+            file_path = "unit/screening/data/test_data.xlsx"
+            ds = DESilico.DictScreeningWithDefault(fitness_dict, default)
+
+            @test typeof(ds) == DESilico.DictScreeningWithDefault
+            for key in keys(fitness_dict)
+                @test ds(key) == fitness_dict[key]
+            end
+            @test ds(missing_key) == default
+        end
+
+        @testset "from Dict" begin
+            ds = DESilico.DictScreening(fitness_dict, default)
+
+            @test typeof(ds) == DESilico.DictScreeningWithDefault
+            for key in keys(fitness_dict)
+                @test ds(key) == fitness_dict[key]
+            end
+            @test ds(missing_key) == default
+        end
+
+        @testset "from file" begin
+            file_path = "unit/screening/data/test_data.xlsx"
+            ds = DESilico.DictScreening(file_path, default)
+
+            @test typeof(ds) == DESilico.DictScreeningWithDefault
+            for key in keys(fitness_dict)
+                @test ds(key) == fitness_dict[key]
+            end
+            @test ds(missing_key) == default
         end
     end
 end
