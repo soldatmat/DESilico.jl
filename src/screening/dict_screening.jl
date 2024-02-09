5e116621b15752f1a0d5cc225d591a5eee145454
soldamatlab <matoussoldat@email.cz>
2024-02-09 14:39:16 +0100

    Add DictScreeningWithDefault

--- src/screening/dict_screening.jl
+++ src/screening/dict_screening.jl
@@ -7,17 +7,15 @@ Can be constructed with dictionary:
     DictScreening(fitness_dict::Dict{<:AbstractVector{Char},<:Real})
 or path to a .xlsx file:
     DictScreening(file_path::String; sheet::int=1, sequence_column::String="Variants", fitness_column::String="Fitness")
+
+Adding a `default::Real` argument to one of the constructers returns a `DictScreeningWithDefault`.
 """
 struct DictScreening <: Screening
     fitness_dict::Dict{<:AbstractVector{Char},<:Real}
 
-    function DictScreening(fitness_dict::Dict{<:AbstractVector{Char},<:Real})
-        new(fitness_dict)
-    end
-
-    function DictScreening(file_path::String; sheet::Int=1, sequence_column::String="Variants", fitness_column::String="Fitness")
+    function load_dict(file_path::String; sheet::Int, sequence_column::String, fitness_column::String)
         xf = XLSX.readxlsx(file_path)
-        dt = XLSX.readtable(file_path, XLSX.sheetnames(xf)[1])
+        dt = XLSX.readtable(file_path, XLSX.sheetnames(xf)[sheet])
 
         variants_idx = dt.column_label_index[Symbol(sequence_column)]
         variants = Vector{String}(dt.data[variants_idx])
@@ -27,9 +25,40 @@ struct DictScreening <: Screening
         fitness_idx = dt.column_label_index[Symbol(fitness_column)]
         fitness = Vector{Real}(dt.data[fitness_idx])
 
-        new(Dict(variants .=> fitness))
+        Dict(variants .=> fitness)
+    end
+
+    function DictScreening(fitness_dict::Dict{<:AbstractVector{Char},<:Real})
+        new(fitness_dict)
+    end
+    function DictScreening(fitness_dict::Dict{<:AbstractVector{Char},<:Real}, default::Real)
+        DictScreeningWithDefault(fitness_dict, default)
+    end
+
+    function DictScreening(file_path::String; sheet::Int=1, sequence_column::String="Variants", fitness_column::String="Fitness")
+        new(load_dict(file_path; sheet, sequence_column, fitness_column))
+    end
+    function DictScreening(file_path::String, default::Real; sheet::Int=1, sequence_column::String="Variants", fitness_column::String="Fitness")
+        DictScreeningWithDefault(load_dict(file_path; sheet, sequence_column, fitness_column), default)
     end
 end
 function (s::DictScreening)(sequence::AbstractVector{Char})
     s.fitness_dict[sequence]
+end
+
+"""
+Uses a sequence-fitness dictionary to simulate the screening.
+
+Can be constructed directly with dictionary and default fitness value:
+    DictScreeningWithDefault(fitness_dict::Dict{<:AbstractVector{Char},<:Real}, default::Real)
+or via `DictScreening` constructors by adding the default fitness value:
+    DictScreening(fitness_dict::Dict{<:AbstractVector{Char},<:Real}, default::Real)
+    DictScreening(file_path::String, default::Real; sheet::Int=1, sequence_column::String="Variants", fitness_column::String="Fitness")
+"""
+struct DictScreeningWithDefault <: Screening
+    fitness_dict::Dict{<:AbstractVector{Char},<:Real}
+    default::Real
+end
+function (s::DictScreeningWithDefault)(sequence::AbstractVector{Char})
+    get(s.fitness_dict, sequence, s.default)
 end
