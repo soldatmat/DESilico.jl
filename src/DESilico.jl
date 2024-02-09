fb5f5e14cfa02e3409f3729ae922fe8c3a0b084c
soldamatlab <matoussoldat@email.cz>
2024-02-09 14:42:17 +0100

    Add alphabet

--- src/DESilico.jl
+++ src/DESilico.jl
@@ -7,6 +7,7 @@ include("de.jl")
 include("selection_strategy/include.jl")
 include("mutagenesis/include.jl")
 include("screening/include.jl")
+include("alphabet.jl")
 include("utils.jl")
 
 end
---
+++ src/alphabet.jl
@@ -0,0 +1,7 @@
+alphabet = (
+    dna=Set(['A', 'G', 'C', 'T']),
+    rna=Set(['A', 'G', 'C', 'U']),
+    dna_extended=Set(['A', 'G', 'C', 'T', 'U']),
+    protein=Set(['A', 'R', 'N', 'D', 'C', 'Q', 'E', 'G', 'H', 'I', 'L', 'K', 'M', 'F', 'P', 'S', 'T', 'W', 'Y', 'V']),
+    protein_extended=Set(['A', 'R', 'N', 'D', 'C', 'Q', 'E', 'G', 'H', 'I', 'L', 'K', 'M', 'F', 'P', 'S', 'T', 'W', 'Y', 'V', 'U', 'O', 'B', 'Z']),
+)
