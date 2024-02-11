8a26747ddf950d561646f70e9309812b2b16de63
soldamatlab <matoussoldat@email.cz>
2024-02-11 15:35:21 +0100

    Add sequence_from_alphabet test

--- test/unit/utils.jl
+++ test/unit/utils.jl
@@ -1,4 +1,19 @@
 @testset "utils.jl" begin
+    @testset "sequence_from_alphabet" begin
+        alphabet = Set(['A', 'B'])
+        @test DESilico.sequence_from_alphabet(Vector{Char}([]), alphabet) == true
+        @test DESilico.sequence_from_alphabet(['B'], alphabet) == true
+        @test DESilico.sequence_from_alphabet(['B', 'A', 'B', 'B'], alphabet) == true
+        @test DESilico.sequence_from_alphabet([' '], alphabet) == false
+        @test DESilico.sequence_from_alphabet(['C'], alphabet) == false
+        @test DESilico.sequence_from_alphabet(['B', 'A', 'C', 'B'], alphabet) == false
+
+        alphabet = Set{Char}([])
+        @test DESilico.sequence_from_alphabet(Vector{Char}([]), alphabet) == true
+        @test DESilico.sequence_from_alphabet([' '], alphabet) == false
+        @test DESilico.sequence_from_alphabet(['A'], alphabet) == false
+    end
+
     @testset "same_length_sequences" begin
         sequences = [['A'], ['A'], ['A']]
         @test DESilico.same_length_sequences(sequences) == true
