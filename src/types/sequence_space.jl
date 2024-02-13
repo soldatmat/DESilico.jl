bbafcb436f8b21a5d5d8ed1ee4c8b47101d8b18d
soldamatlab <matoussoldat@email.cz>
2024-02-13 14:37:09 +0100

    Add SequenceSpace{Vector{Variant}}

--- src/types/sequence_space.jl
+++ src/types/sequence_space.jl
@@ -7,15 +7,24 @@ mutable struct SequenceSpace{T}
     top_variant::Variant
 
     SequenceSpace{T}(population, variants, top_variant) where {T} = new{T}(population, variants, top_variant)
+    SequenceSpace(population, variants::T, top_variant) where {T} = new{T}(population, variants, top_variant)
 
-    SequenceSpace(population, variants, top_variant) = new{Set{Variant}}(population, variants, top_variant)
-    SequenceSpace(variants::Set{Variant}) = new{Set{Variant}}([v.sequence for v in collect(variants)], variants, get_top_variant(collect(variants)))
-    SequenceSpace(variants::AbstractVector{Variant}) = new{Set{Variant}}([v.sequence for v in variants], Set(variants), get_top_variant(variants))
+    SequenceSpace(variants::Set{Variant}) = SequenceSpace{Set{Variant}}(variants)
+    SequenceSpace(variants::AbstractVector{Variant}) = SequenceSpace{Set{Variant}}(variants)
+
+    SequenceSpace{Set{Variant}}(variants::Set{Variant}) = new{Set{Variant}}([v.sequence for v in collect(variants)], variants, get_top_variant(collect(variants)))
+    SequenceSpace{Set{Variant}}(variants::AbstractVector{Variant}) = new{Set{Variant}}([v.sequence for v in variants], Set(variants), get_top_variant(variants))
+
+    SequenceSpace{Vector{Variant}}(variants::Set{Variant}) = new{Vector{Variant}}([v.sequence for v in collect(variants)], collect(variants), get_top_variant(collect(variants)))
+    SequenceSpace{Vector{Variant}}(variants::AbstractVector{Variant}) = new{Vector{Variant}}([v.sequence for v in variants], variants, get_top_variant(variants))
 
     get_top_variant(variants::AbstractVector{Variant}) = sort(variants, by=x -> x.fitness, rev=true)[1]
 end
 
 function update_variants!(ss::SequenceSpace{Set{Variant}}, variants::AbstractVector{Variant})
+    map(variant -> push!(ss.variants, variant), variants)
+end
+function update_variants!(ss::SequenceSpace{Vector{Variant}}, variants::AbstractVector{Variant})
     map(variant -> push!(ss.variants, variant), variants)
 end
 
--- test/unit/types/sequence_space.jl
+++ test/unit/types/sequence_space.jl
@@ -1,36 +1,112 @@
 @testset "sequence_space.jl" begin
-    @testset "basic constructor" begin
-        variants = [Variant(['A'], 0), Variant(['B'], 1)]
-        ss = SequenceSpace{Set{Variant}}([['A'], ['B']], Set(variants), variants[2])
+    @testset "constructors" begin
+        @testset "SequeneSpace{Set{Variant}}" begin
+            @testset "basic constructor" begin
+                @testset "explicit" begin
+                    variants = [Variant(['A'], 0), Variant(['B'], 1)]
+                    ss = SequenceSpace{Set{Variant}}([['A'], ['B']], Set(variants), variants[2])
 
-        @test typeof(ss) == SequenceSpace{Set{Variant}}
-        @test length(ss.population) == 2
-        @test Set(ss.population) == Set([['A'], ['B']])
-        @test typeof(ss.variants) == Set{Variant}
-        @test ss.variants == Set(variants)
-        @test isequal(ss.top_variant, variants[2])
-    end
+                    @test typeof(ss) == SequenceSpace{Set{Variant}}
+                    @test length(ss.population) == 2
+                    @test Set(ss.population) == Set([['A'], ['B']])
+                    @test typeof(ss.variants) == Set{Variant}
+                    @test ss.variants == Set(variants)
+                    @test isequal(ss.top_variant, variants[2])
+                end
+                @testset "implicit" begin
+                    variants = [Variant(['A'], 0), Variant(['B'], 1)]
+                    ss = SequenceSpace([['A'], ['B']], Set(variants), variants[2])
 
-    @testset "vector constructor" begin
-        ss = SequenceSpace([Variant(['A'], 0), Variant(['B'], 1)])
+                    @test typeof(ss) == SequenceSpace{Set{Variant}}
+                    @test length(ss.population) == 2
+                    @test Set(ss.population) == Set([['A'], ['B']])
+                    @test typeof(ss.variants) == Set{Variant}
+                    @test ss.variants == Set(variants)
+                    @test isequal(ss.top_variant, variants[2])
+                end
+            end
 
-        @test typeof(ss) == SequenceSpace{Set{Variant}}
-        @test length(ss.population) == 2
-        @test Set(ss.population) == Set([['A'], ['B']])
-        @test typeof(ss.variants) == Set{Variant}
-        @test ss.variants == Set([Variant(['A'], 0), Variant(['B'], 1)])
-        @test isequal(ss.top_variant, Variant(['B'], 1))
-    end
+            @testset "vector constructor" begin
+                ss = SequenceSpace{Set{Variant}}([Variant(['A'], 0), Variant(['B'], 1)])
 
-    @testset "set constructor" begin
-        ss = SequenceSpace(Set([Variant(['A'], 0), Variant(['B'], 1)]))
+                @test typeof(ss) == SequenceSpace{Set{Variant}}
+                @test length(ss.population) == 2
+                @test Set(ss.population) == Set([['A'], ['B']])
+                @test typeof(ss.variants) == Set{Variant}
+                @test ss.variants == Set([Variant(['A'], 0), Variant(['B'], 1)])
+                @test isequal(ss.top_variant, Variant(['B'], 1))
+            end
 
-        @test typeof(ss) == SequenceSpace{Set{Variant}}
-        @test length(ss.population) == 2
-        @test Set(ss.population) == Set([['A'], ['B']])
-        @test typeof(ss.variants) == Set{Variant}
-        @test ss.variants == Set([Variant(['A'], 0), Variant(['B'], 1)])
-        @test isequal(ss.top_variant, Variant(['B'], 1))
+            @testset "set constructor" begin
+                ss = SequenceSpace{Set{Variant}}(Set([Variant(['A'], 0), Variant(['B'], 1)]))
+
+                @test typeof(ss) == SequenceSpace{Set{Variant}}
+                @test length(ss.population) == 2
+                @test Set(ss.population) == Set([['A'], ['B']])
+                @test typeof(ss.variants) == Set{Variant}
+                @test ss.variants == Set([Variant(['A'], 0), Variant(['B'], 1)])
+                @test isequal(ss.top_variant, Variant(['B'], 1))
+            end
+        end
+
+        @testset "SequeneSpace{Vector{Variant}}" begin
+            @testset "basic constructor" begin
+                @testset "explicit" begin
+                    variants = [Variant(['A'], 0), Variant(['B'], 1)]
+                    ss = SequenceSpace{Vector{Variant}}([['A'], ['B']], variants, variants[2])
+
+                    @test typeof(ss) == SequenceSpace{Vector{Variant}}
+                    @test length(ss.population) == 2
+                    @test Set(ss.population) == Set([['A'], ['B']])
+                    @test typeof(ss.variants) == Vector{Variant}
+                    @test ss.variants == variants
+                    @test isequal(ss.top_variant, variants[2])
+                end
+                @testset "implicit" begin
+                    variants = [Variant(['A'], 0), Variant(['B'], 1)]
+                    ss = SequenceSpace([['A'], ['B']], variants, variants[2])
+
+                    @test typeof(ss) == SequenceSpace{Vector{Variant}}
+                    @test length(ss.population) == 2
+                    @test Set(ss.population) == Set([['A'], ['B']])
+                    @test typeof(ss.variants) == Vector{Variant}
+                    @test ss.variants == variants
+                    @test isequal(ss.top_variant, variants[2])
+                end
+            end
+
+            @testset "vector constructor" begin
+                ss = SequenceSpace{Vector{Variant}}([Variant(['A'], 0), Variant(['B'], 1)])
+
+                @test typeof(ss) == SequenceSpace{Vector{Variant}}
+                @test length(ss.population) == 2
+                @test Set(ss.population) == Set([['A'], ['B']])
+                @test typeof(ss.variants) == Vector{Variant}
+                @test isequal(ss.variants, [Variant(['A'], 0), Variant(['B'], 1)])
+                @test isequal(ss.top_variant, Variant(['B'], 1))
+            end
+
+            @testset "set constructor" begin
+                ss = SequenceSpace{Vector{Variant}}(Set([Variant(['A'], 0), Variant(['B'], 1)]))
+
+                @test typeof(ss) == SequenceSpace{Vector{Variant}}
+                @test length(ss.population) == 2
+                @test Set(ss.population) == Set([['A'], ['B']])
+                @test typeof(ss.variants) == Vector{Variant}
+                @test isequal(ss.variants, [Variant(['A'], 0), Variant(['B'], 1)])
+                @test isequal(ss.top_variant, Variant(['B'], 1))
+            end
+        end
+
+        @testset "default parameter" begin
+            variants = [Variant(['A'], 0)]
+
+            ss = SequenceSpace(variants)
+            @test typeof(ss) == SequenceSpace{Set{Variant}}
+
+            ss = SequenceSpace(Set(variants))
+            @test typeof(ss) == SequenceSpace{Set{Variant}}
+        end
     end
 
     @testset "update_top_variant!" begin
@@ -64,6 +140,12 @@
             ss = SequenceSpace([Variant(['A'], 0), Variant(['B'], 1)])
             DESilico.update_variants!(ss, [Variant(['C'], 1.5), Variant(['B'], 1.5), Variant(['D'], -1)])
             @test ss.variants == Set([Variant(['A'], 0), Variant(['B'], 1), Variant(['C'], 1.5), Variant(['D'], -1)])
+        end
+
+        @testset "SequenceSpace{Vector{Variant}}" begin
+            ss = SequenceSpace{Vector{Variant}}([Variant(['A'], 0), Variant(['B'], 1)])
+            DESilico.update_variants!(ss, [Variant(['C'], 1.5), Variant(['B'], 1.5), Variant(['D'], -1)])
+            @test isequal(ss.variants, [Variant(['A'], 0), Variant(['B'], 1), Variant(['C'], 1.5), Variant(['B'], 1.5), Variant(['D'], -1)])
         end
     end
 
