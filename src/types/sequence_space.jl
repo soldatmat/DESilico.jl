da2315f0eb44a8ed5c9e28161ad8eff01f7e096f
soldamatlab <matoussoldat@email.cz>
2024-02-13 13:44:42 +0100

    Refactor SequenceSpace

--- src/types/sequence_space.jl
+++ src/types/sequence_space.jl
@@ -1,23 +1,32 @@
 """
 TODO
 """
-mutable struct SequenceSpace
+mutable struct SequenceSpace{T}
     population::Vector{Vector{Char}}
-    variants::Set{Variant}
+    variants::T
     top_variant::Variant
 
-    SequenceSpace(population, variants, top_variant) = new(population, variants, top_variant)
-    SequenceSpace(variants::Set{Variant}) = new([v.sequence for v in collect(variants)], variants, get_top_variant(collect(variants)))
-    SequenceSpace(variants::AbstractVector{Variant}) = new([v.sequence for v in variants], Set(variants), get_top_variant(variants))
+    SequenceSpace{T}(population, variants, top_variant) where {T} = new{T}(population, variants, top_variant)
+
+    SequenceSpace(population, variants, top_variant) = new{Set{Variant}}(population, variants, top_variant)
+    SequenceSpace(variants::Set{Variant}) = new{Set{Variant}}([v.sequence for v in collect(variants)], variants, get_top_variant(collect(variants)))
+    SequenceSpace(variants::AbstractVector{Variant}) = new{Set{Variant}}([v.sequence for v in variants], Set(variants), get_top_variant(variants))
 
     get_top_variant(variants::AbstractVector{Variant}) = sort(variants, by=x -> x.fitness, rev=true)[1]
 end
 
+function update_variants!(ss::SequenceSpace{Set{Variant}}, variants::AbstractVector{Variant})
+    map(variant -> push!(ss.variants, variant), variants)
+end
+
+function update_top_variant!(ss::SequenceSpace, variant::Variant)
+    variant.fitness > ss.top_variant.fitness && (ss.top_variant = variant)
+end
+function update_top_variant!(ss::SequenceSpace, variants::AbstractVector{Variant})
+    map(variant -> update_top_variant!(ss, variant), variants)
+end
+
 function push_variants!(ss::SequenceSpace, variants::AbstractVector{Variant})
-    for variant in variants
-        push!(ss.variants, variant)
-        if variant.fitness > ss.top_variant.fitness
-            ss.top_variant = copy(variant)
-        end
-    end
+    update_variants!(ss, variants)
+    update_top_variant!(ss, variants)
 end
--- test/unit/types/sequence_space.jl
+++ test/unit/types/sequence_space.jl
@@ -1,10 +1,23 @@
 @testset "sequence_space.jl" begin
+    @testset "basic constructor" begin
+        variants = [Variant(['A'], 0), Variant(['B'], 1)]
+        ss = SequenceSpace{Set{Variant}}([['A'], ['B']], Set(variants), variants[2])
+
+        @test typeof(ss) == SequenceSpace{Set{Variant}}
+        @test length(ss.population) == 2
+        @test Set(ss.population) == Set([['A'], ['B']])
+        @test typeof(ss.variants) == Set{Variant}
+        @test ss.variants == Set(variants)
+        @test isequal(ss.top_variant, variants[2])
+    end
+
     @testset "vector constructor" begin
         ss = SequenceSpace([Variant(['A'], 0), Variant(['B'], 1)])
 
-        @test typeof(ss) == SequenceSpace
+        @test typeof(ss) == SequenceSpace{Set{Variant}}
         @test length(ss.population) == 2
         @test Set(ss.population) == Set([['A'], ['B']])
+        @test typeof(ss.variants) == Set{Variant}
         @test ss.variants == Set([Variant(['A'], 0), Variant(['B'], 1)])
         @test isequal(ss.top_variant, Variant(['B'], 1))
     end
@@ -12,11 +25,46 @@
     @testset "set constructor" begin
         ss = SequenceSpace(Set([Variant(['A'], 0), Variant(['B'], 1)]))
 
-        @test typeof(ss) == SequenceSpace
+        @test typeof(ss) == SequenceSpace{Set{Variant}}
         @test length(ss.population) == 2
         @test Set(ss.population) == Set([['A'], ['B']])
+        @test typeof(ss.variants) == Set{Variant}
         @test ss.variants == Set([Variant(['A'], 0), Variant(['B'], 1)])
         @test isequal(ss.top_variant, Variant(['B'], 1))
+    end
+
+    @testset "update_top_variant!" begin
+        @testset "single variant" begin
+            ss = SequenceSpace([Variant(['A'], 0), Variant(['B'], 1)])
+            DESilico.update_top_variant!(ss, Variant(['C'], 0.5))
+            @test isequal(ss.top_variant, Variant(['B'], 1))
+
+            ss = SequenceSpace([Variant(['A'], 0), Variant(['B'], 1)])
+            DESilico.update_top_variant!(ss, Variant(['C'], 1.5))
+            @test isequal(ss.top_variant, Variant(['C'], 1.5))
+        end
+
+        @testset "mutliple variants" begin
+            ss = SequenceSpace([Variant(['A'], 0), Variant(['B'], 1)])
+            DESilico.update_top_variant!(ss, [Variant(['C'], 0.5), Variant(['D'], 0.6)])
+            @test isequal(ss.top_variant, Variant(['B'], 1))
+
+            ss = SequenceSpace([Variant(['A'], 0), Variant(['B'], 1)])
+            DESilico.update_top_variant!(ss, [Variant(['C'], 1.5), Variant(['D'], 2.0)])
+            @test isequal(ss.top_variant, Variant(['D'], 2.0))
+
+            ss = SequenceSpace([Variant(['A'], 0), Variant(['B'], 1)])
+            DESilico.update_top_variant!(ss, [Variant(['C'], 2.0), Variant(['D'], 1.5)])
+            @test isequal(ss.top_variant, Variant(['C'], 2.0))
+        end
+    end
+
+    @testset "update_variants!" begin
+        @testset "SequenceSpace{Set{Variant}}" begin
+            ss = SequenceSpace([Variant(['A'], 0), Variant(['B'], 1)])
+            DESilico.update_variants!(ss, [Variant(['C'], 1.5), Variant(['B'], 1.5), Variant(['D'], -1)])
+            @test ss.variants == Set([Variant(['A'], 0), Variant(['B'], 1), Variant(['C'], 1.5), Variant(['D'], -1)])
+        end
     end
 
     @testset "push_variants!" begin
